63cbe4a4cfba2df8970fa35652f9096de713dc51
Kristof Mariën <krmarien@gmail.com>
2015-06-14 11:37:12 +0200

    Forgot 2 files

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_VERSION='/api/v1'
 CSRF_ENABLED = True
 SECRET_KEY = 'ba0b875d-aa57-4574-b05e-3c9c825a2bbf'
 
-SQLALCHEMY_DATABASE_URI = 'pgsql://zwayrest:zwayrest@localhost/zwayrest'
+SQLALCHEMY_DATABASE_URI = 'postgresql://zwayrest:zwayrest@localhost/zwayrest'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 60*60*24*30
